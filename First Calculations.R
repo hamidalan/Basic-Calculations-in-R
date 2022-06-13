e02153b630971d2b93e6dabcac31ef42b9a5abf3
HAMIDA ABDUL-KHALIQ <77174969+hamidalan@users.noreply.github.com>
2022-06-12 19:16:20 -0500

    First Commit

---
+++ First Calculations.R
@@ -0,0 +1,12 @@
+# my first calculations
+# assigning the variable quarter_1_sales and quarter_2_sales with values
+quarter_1_sales <- 20
+quarter_2_sales <- 50
+
+# using addition for to find midyear_sales
+midyear_sales <- quarter_1_sales + quarter_2_sales
+midyear_sales
+
+# using multiplication to find yearend_sales
+yearend_sales <- midyear_sales * 2
+yearend_sales
